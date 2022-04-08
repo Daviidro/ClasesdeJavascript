ba33756a350b4cbbd088f114e6714d721f089b68
Daviidro <arq.drs23@gmail.com>
2022-04-07 21:41:33 -0500

    clase de Java

---
+++ js/file.js
@@ -0,0 +1,60 @@
+
+/*
+let nombre; 
+console.log (Boolean(nombre));
+
+if (nombre) {
+    console.log("entro el caso verdadero");
+} else {
+    console.log("entro el caso falso");
+} 
+*/
+
+
+/*
+let nombre = "David" ;
+let apellido ;
+let segundoApellido ;
+
+
+if (!nombre) {
+    console.log("es verdadero");
+} else {
+    console.log("Es falso");
+}
+*/
+
+/* 
+
+
+
+
+
+
+/*
+if (nombre || apellido || segundoApellido) {
+    console.log("el usuario si tiene nombre");
+} else {
+    console.log("no hay datos");
+}
+*/
+
+
+
+
+
+
+/*
+console.log (Boolean(nombre));
+
+if (nombre) {
+    console.log("si existe la variable nombre");
+} else if (apellido) {
+    console.log("si existe apellido");
+} else if (segundoApellido) {
+    console.log("si existe segundo apellido"); 
+} else {
+    console.log("no existe ningun dato"); 
+}
+
+*/
